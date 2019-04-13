9c9c30c1c49bf6f078242d775d2c62b12ca3286b
73zillion <73zillion@gmail.com>
2019-04-13 15:03:27 +0900

    add mailchimp api

--- Newsletter-Signup/app.js
+++ Newsletter-Signup/app.js
@@ -16,9 +16,45 @@ app.post("/", function (req, res) {
     var lastName = req.body.lastname;
     var email = req.body.email;
 
-    console.log(firstName, lastName);
+    var data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    var jsonData = JSON.stringify(data);
+
+    var options = {
+        url: "https://us20.api.mailchimp.com/3.0/lists/42f9bd2b1a",
+        method: "POST",
+        headers: {
+            "Authorization": "73zillion 447350477f0f6ab23fcdaa5756105d7c-us20"
+        },
+        body: jsonData
+    };
+
+    request(options, function (error, response, body) {
+       if (error) {
+           console.log(error);
+       } else {
+           console.log(response.statusCode);
+       }
+    });
 });
 
 app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
+
+// API id
+// 447350477f0f6ab23fcdaa5756105d7c-us20
+
+// List id
+// 42f9bd2b1a
